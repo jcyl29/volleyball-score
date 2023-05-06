60309a93ff6a3d0ffb2d3d64785b0141c98f6f8b
Justin Lui <jlui@momentive.ai>
2023-05-06 12:24:41 -0700

    functionality added

--- scorer.js
+++ scorer.js
@@ -4,15 +4,53 @@ export default class Scorer {
             team1: 0,
             team2: 0
         }
+        this.scoreRange = {
+            min: 0,
+            max: 25
+        }
         console.log(this.scores)
         this.bindEvents()
+        this.renderScores()
     }
 
-    bindEvents () {
+
+    bindEvents() {
         const buttons = Array.from(document.getElementsByClassName('digit'));
         buttons.forEach(b => {
-            b.addEventListener('click', (e) => console.log(e.target, e.target.classList))
+            b.addEventListener('click', this.updateScore.bind(this))
         })
+    }
 
+    updateScore(e) {
+        const {target: {dataset}} = e
+        // debugger
+        const action = dataset.action
+        const team = dataset.team
+        console.log('dataset', dataset, action, team)
+        const action2Method = {
+            add: () => {
+                this.scores[team] < this.scoreRange.max ? this.scores[team]++ : this.scores[team]
+            },
+            minus: () => {
+                this.scores[team] > this.scoreRange.min ? this.scores[team]-- : this.scores[team]
+            }
+        }
+        action2Method[action]()
+        this.renderScores()
+    }
+
+    renderScores() {
+        const score1Str = this.scores.team1.toString().padStart(2, '0')
+        const score2Str = this.scores.team2.toString().padStart(2, '0')
+        console.log(score1Str, score2Str)
+        const team1Digit1 = document.querySelector('[data-team="team1"][data-action="minus"]')
+        const team1Digit2 = document.querySelector('[data-team="team1"][data-action="add"]')
+        const team2Digit1 = document.querySelector('[data-team="team2"][data-action="minus"]')
+        const team2Digit2 = document.querySelector('[data-team="team2"][data-action="add"]')
+
+        team1Digit1.innerText = score1Str.charAt(0);
+        team1Digit2.innerText = score1Str.charAt(1);
+        team2Digit1.innerText = score2Str.charAt(0);
+        team2Digit2.innerText = score2Str.charAt(1);
     }
 }
